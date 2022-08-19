e9ed182510173b279f66983e7b4e72582a4a5b7b
akxhdev <akxhdev@gmail.com>
2022-08-19 14:10:51 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pricing-ui';
+
+  innerWidth: number;
+
+  constructor() {
+    // get inner width
+    this.innerWidth = window.innerWidth;
+  }
 }
