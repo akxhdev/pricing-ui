28aafb62b0d53a9284c314c051b912d7a28bbf96
akxhdev <akxhdev@gmail.com>
2022-08-19 14:13:42 +0530

    add docs folder

---
+++ src/app/components/price-card/price-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'price-card',
+  templateUrl: './price-card.component.html',
+  styleUrls: ['./price-card.component.css'],
+})
+export class PriceCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
